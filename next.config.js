813ec6855bcff8455e7e681c6cf7ab200b219b0a
Mariosat15 <cybestbuyit@gmail.com>
2026-02-27 09:04:17 +0200

    fix(build): add explicit webpack alias for @/ to fix server builds

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
   images: {
     remotePatterns: [
       {
